43676882201ad1f73d614da31e4f47d3af0da288
陈黔 <chenq1@bsoft.com.cn>
2020-10-13 16:26:18 +0800

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by BlackChen on 2020/10/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
