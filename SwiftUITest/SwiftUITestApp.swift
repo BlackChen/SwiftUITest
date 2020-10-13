54b4caf08e9786f88b8d5865306b07626735cd65
陈黔 <chenq1@bsoft.com.cn>
2020-10-13 16:40:50 +0800

    提交

--- SwiftUITest/SwiftUITestApp.swift
+++ SwiftUITest/SwiftUITestApp.swift
@@ -15,3 +15,9 @@ struct SwiftUITestApp: App {
         }
     }
 }
+
+struct SwiftUITestApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
