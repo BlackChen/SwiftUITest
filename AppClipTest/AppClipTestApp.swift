65c43903c445b6fb38c58c8ace1dba54bdab79d9
陈黔 <chenq1@bsoft.com.cn>
2020-10-13 16:40:03 +0800

    提交

---
+++ AppClipTest/AppClipTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipTestApp.swift
+//  AppClipTest
+//
+//  Created by BlackChen on 2020/10/13.
+//
+
+import SwiftUI
+
+@main
+struct AppClipTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppClipTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppClipTest
+//
+//  Created by BlackChen on 2020/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
